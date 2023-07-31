269edc2b86204bc2c56d8a39ead462e71caa6481
ewouds <88662820+ewouds@users.noreply.github.com>
2023-07-31 08:28:44 +0000

    member.js

--- member.js
+++ member.js
@@ -1,10 +1,26 @@
-function skillMembers() {
-    var members = document.getElementsByClassName("member");
-    for (var i = 0; i < members.length; i++) {
-        members[i].style.display = "none";
-    }
-    var skills = document.getElementsByClassName("skill");
-    for (var i = 0; i < skills.length; i++) {
-        skills[i].style.display = "block";
-    }
+function skillsMember() {
+  const member = document.getElementById('member');
+  if (member) {
+    const skill = document.querySelectorAll('.skill');
+    const skillName = document.querySelectorAll('.skill-name');
+    const skillValue = document.querySelectorAll('.skill-value');
+    const skillBar = document.querySelectorAll('.skill-bar');
+
+    skill.forEach((item, index) => {
+      item.addEventListener('mouseover', () => {
+        skill[index].classList.add('skill-active');
+        skillName[index].classList.add('skill-name-active');
+        skillValue[index].classList.add('skill-value-active');
+        skillBar[index].classList.add('skill-bar-active');
+      });
+      item.addEventListener('mouseout', () => {
+        skill[index].classList.remove('skill-active');
+        skillName[index].classList.remove('skill-name-active');
+        skillValue[index].classList.remove('skill-value-active');
+        skillBar[index].classList.remove('skill-bar-active');
+      });
+    });
+  }
 }
+    
+    
